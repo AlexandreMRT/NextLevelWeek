63eb79e5ff1f35f629eebaf294ee6eaebf3898f7
Alexandre Teixeira <alexandre@loldesign.com.br>
2020-06-06 14:28:08 -0300

    Add map to the form

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+import { Map, TileLayer, Marker } from 'react-leaflet';
 
 import './styles.css';
 import logo from '../../assets/logo.svg'
@@ -62,6 +63,15 @@ const CreatePoint = () => {
             <h2>Endereço</h2>
             <span>Selecione o endereço no mapa</span>
           </legend>
+
+          <Map center={[-23.644548, -46.7468855]} zoom={15} >
+            <TileLayer
+              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+
+            <Marker position={[-23.644548, -46.7468855]} zoom={15} />
+          </Map>
 
           <div className="field-group">
             <div className="field">
