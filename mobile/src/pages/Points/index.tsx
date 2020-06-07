895dda4a120bec6724a94f61a9b5a641769fcaeb
Alexandre Teixeira <alexandre@loldesign.com.br>
2020-06-07 16:10:33 -0300

    Add condition to only open map after permission and when get user location

--- mobile/src/pages/Points/index.tsx
+++ mobile/src/pages/Points/index.tsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, Text, ScrollView, Image } from 'react-native';
+import { View, StyleSheet, Text, ScrollView, Image, Alert } from 'react-native';
 import Constants from 'expo-constants';
 import { Feather as Icon } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import MapView, { Marker } from 'react-native-maps';
 import { SvgUri } from 'react-native-svg';
+import * as Location from 'expo-location'
 import api from '../../services/api';
 
 interface Item {
@@ -19,11 +20,33 @@ const Points = () => {
   const [selectedItems, setSelectedItems] = useState<number[]>([]);
   const navigation = useNavigation();
 
+  const [initialPosition, setInitialPosition] = useState<[number, number]>([0, 0])
+
 
   useEffect(() => {
     api.get('items').then(response => {
       setItems(response.data);
     })
+  }, []);
+
+  useEffect(() => {
+    async function loadPosition(){
+      const { status } = await Location.requestPermissionsAsync();
+
+      if (status !== 'granted') {
+        Alert.alert('Ooppps....', 'Precisamos de sua permissão para obter localização')
+        return;
+      }
+
+      const location = await Location.getCurrentPositionAsync();
+
+      const { latitude, longitude } = location.coords;
+
+      setInitialPosition([latitude, longitude]);
+    }
+
+
+    loadPosition();
   }, []);
 
 function handleNavigateBack() {
@@ -56,29 +79,31 @@ function handleSelectItem(id: number) {
         <Text style={styles.description}>Encontre no mapa um ponto de coleta</Text>
 
         <View style={styles.mapContainer}>
-          <MapView
-            style={styles.map}
-            initialRegion={{
-              latitude: -23.6449068,
-              longitude: -46.7461992,
-              latitudeDelta: 0.025,
-              longitudeDelta: 0.025,
-            }}
-          >
-            <Marker
-              style={styles.mapMarker}
-              onPress={handleNavigateToDetail}
-              coordinate={{
-                latitude: -23.6449068,
-                longitude: -46.7461992,
+          { initialPosition[0] !== 0 && (
+              <MapView
+              style={styles.map}
+              initialRegion={{
+                latitude: initialPosition[0],
+                longitude: initialPosition[1],
+                latitudeDelta: 0.025,
+                longitudeDelta: 0.025,
               }}
             >
-              <View style={styles.mapMarkerContainer}>
-                <Image style={styles.mapMarkerImage} source={{ uri: "https://images.unsplash.com/photo-1498579397066-22750a3cb424?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60" }}/>
-                <Text style={styles.mapMarkerTitle}>Mercado</Text>
-              </View>
-            </Marker>
-          </MapView>
+              <Marker
+                style={styles.mapMarker}
+                onPress={handleNavigateToDetail}
+                coordinate={{
+                  latitude: -23.6449068,
+                  longitude: -46.7461992,
+                }}
+              >
+                <View style={styles.mapMarkerContainer}>
+                  <Image style={styles.mapMarkerImage} source={{ uri: "https://images.unsplash.com/photo-1498579397066-22750a3cb424?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60" }}/>
+                  <Text style={styles.mapMarkerTitle}>Mercado</Text>
+                </View>
+              </Marker>
+            </MapView>
+          ) }
         </View>
 
       </View>
